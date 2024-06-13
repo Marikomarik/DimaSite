137ef5aa0c05586182bb2ac44bab08706f71de2f
Marikomarik <52992139+Marikomarik@users.noreply.github.com>
2024-06-14 00:09:56 +0700

    1

--- src/Header.js
+++ src/Header.js
@@ -4,47 +4,45 @@ import './Header.css';
 import { useState } from 'react';
 
 export default function Header(){
-    const [isOpen11, setOpen11] = useState(false);
-    const [isOpen12, setOpen12] = useState(false);
-    const [isOpen21, setOpen21] = useState(false);
-    const [isOpen22, setOpen22] = useState(false);
     return(
-    <header>
-        <div className='header'>
-            <a href="" className='logo'><img src={logo} alt=""/></a>
-            <div className='nav'>
-                <ul>
-                    <li onMouseEnter={() => setOpen11(true)} onMouseLeave={() => setTimeout(()=> setOpen11(false), 50)}><a href="" className={`Strela  a-int`}>
-                        <div onMouseEnter={() => setOpen12(true)} onMouseLeave={() => setOpen12(false)} className={`${isOpen11 || isOpen12 ? "active" : ""} popUpMenu`}>
-                            <a>Profile</a>
-                            <a>Members</a>
-                        </div>
-                        Community</a>
-                    </li>
-                    <li><a href="" className='a-int'>Classifieds</a></li>
-                    <li><a href="" className='a-int'>Blog</a></li>
-                    <li onMouseEnter={() => setOpen21(true)} onMouseLeave={() => setTimeout(()=> setOpen21(false), 50)}><a href="" className={`Strela  a-int`}>
-                        <div onMouseEnter={() => setOpen22(true)} onMouseLeave={() => setOpen22(false)} className={`${isOpen21 || isOpen22 ? "active" : ""} popUpMenu`}>
-                            <a>2 Columns</a>
-                            <a>3 Columns</a>
-                            <a>Single Product</a>
-                            <a>Cart</a>
-                            <a>Checkout</a>
-                        </div>
-                        Shop</a>
-                    </li>
-                </ul>
+    <header> 
+        <div className='header-top'>
+            <div className='container flex-between'>
+            <div className='header-top-left'>
+                <div className='header-top-left-tel'>
+                    +800-123-4567 6587
+                </div>
+                <div className='header-top-left-adres'>
+                    Beverley, New York 224 USA
+                </div>
             </div>
-            <div className='div-search'>
-                <form method='get' className='search'>
-                <input className='search-text' type="text" placeholder='Search'></input>
-                <button className='search-button'><img src=""></img></button>
-                </form>
+            
+            <div className='header-top-right'>
+                <div className='header-top-right-text'>
+                    <span>Find us on :</span>
+                </div>
+                <a className='header-top-right-ob'>
+                    a
+                </a>
+                <a className='header-top-right-ob'>
+                    b        
+                </a>
+                <a className='header-top-right-ob'>
+                    c   
+                </a>
+                <a className='header-top-right-ob'>
+                    e
+                </a>
+                <a className='header-top-right-ob'>
+                    f
+                </a>
             </div>
-            <a className='a-int shop'>
-                <img src=""></img>
-                B
-            </a>
+            </div>
+        </div>
+        <div className='header-bottom'>
+            <div className='container flex-between'>
+                
+            </div>
         </div>
     </header>
     )
